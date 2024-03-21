8d047abb520131c05f35bf43bb1087e5f564a27f
Raghav B. Venkataramaiyer <bvraghav@gmail.com>
2024-03-21 06:59:53 +0530

    Add experiment details.

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 function main() {
+  const experiment = new Experiment()
+
+  // Update Candidate Details
+  updateCandidateDetails(Experiment)
+
   const lineClipConfigs = [
     {
       sel: "#canvas-sutherland-cohen",
@@ -12,5 +17,30 @@ function main() {
 
   for (const {sel, clip} of lineClipConfigs) {
     new LineClipSetup(sel, clip).draw()
+  }
+}
+
+function updateCandidateDetails({rollNo,name}) {
+  let isValidRollNo, isValidName
+  isValidRollNo = (isValidName = false)
+
+  // Validate RollNo
+  rollNo = Number(rollNo)
+  isValidRollNo = !isNaN(rollNo) && 9999999 < rollNo
+  if (!isValidRollNo) {
+    console.warn({invalidRollNo: rollNo})
+  }
+
+  // Validate Name
+  const titleCasePat = /^[A-Z][a-z]+( [A-Z][a-z]+)*$/
+  name = String(name).trim()
+  isValidName = titleCasePat.test(name)
+  if (!isValidName) {
+    console.warn({invalidName: name})
+  }
+  
+  if (isValidRollNo && isValidName) {
+    document.querySelector('#by')
+      .textContent = `By: ${name} (${rollNo})`
   }
 }
